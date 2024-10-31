21a1a27d9cf61ebdb8aeee172b0de70e70ffb238
Sathvik Bhavaraju <sathvikb21@gmail.com>
2024-10-31 14:07:27 -0400

    Configured Routing

--- phoenix_ng/src/app/app.routes.ts
+++ phoenix_ng/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ExpoPageComponent } from './expo-page/expo-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AlumniPageComponent } from './alumni-page/alumni-page.component';
+import { CareerFairPageComponent } from './career-fair-page/career-fair-page.component';
+import { ExecMainPageComponent } from './exec-main-page/exec-main-page.component';
+import { ExecPageComponent } from './exec-page/exec-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'alumni', component: AlumniPageComponent },
+    { path: 'expo', component: ExpoPageComponent },
+    { path: 'career-fair', component: CareerFairPageComponent },
+    { path: 'exec', component: ExecMainPageComponent },
+    { path: 'exec-info', component: ExecPageComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' } // Wildcard route for a 404 page (optional)
+];
