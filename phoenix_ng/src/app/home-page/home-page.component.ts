02d44ff1bcb078e4b7f0d6604166e274672d49e3
Vishesh <anandvh@mail.uc.edu>
2024-11-04 13:56:24 -0500

    feat: :sparkles: Home Page UI
    Added the basic UI for the home-page
    
    Exec Board picture, Google Calendar Integration, and UI changes pending

--- phoenix_ng/src/app/home-page/home-page.component.ts
+++ phoenix_ng/src/app/home-page/home-page.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
 
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './home-page.component.html',
 })
+
 export class HomePageComponent {
 
 }
