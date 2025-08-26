60f86d3ce4a09b9d2e00eb21ab136424965e7e3e
sohamvakani <sohamvakani@gmail.com>
2025-08-26 17:49:43 -0400

    adding reimbursement page, removing alumni block

---
+++ phoenix_ng/src/app/reimbursement/reimbursement-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reimbursement',
+  standalone: true,
+  imports: [],
+  templateUrl: './reimbursement-page.component.html',
+})
+export class ReimbursementPageComponent {}
